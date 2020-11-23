69b892360f412e2e4f461415d2571efe38929fec
Alan-eMartin <alan.edward.martin@gmail.com>
2020-11-23 02:28:48 -0500

    fixed margins

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,12 @@
 import '../Sass/App.scss'
 import Hero from '../Components/Hero/Hero'
-import Nav from "../Components/Nav/Nav"
 
 const App = () => {
   return (
-    <div>
+    <div className='App'>
+      <main>
       <Hero />
+      </main>
     </div>
   )
 }
